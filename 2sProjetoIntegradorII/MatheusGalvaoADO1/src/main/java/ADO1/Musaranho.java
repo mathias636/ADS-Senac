ea1b4df08713b9811563909d73d42a4aa9e030e8
MATHEUS GALVAO SANTOS <matheus.gsantos21@senacsp.edu.br>
2022-03-04 22:17:51 -0300

    save

---
+++ 2sProjetoIntegradorII/MatheusGalvaoADO1/src/main/java/ADO1/Musaranho.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ADO1;
+
+/**
+ *
+ * @author matheus.gsantos21
+ */
+public class Musaranho {
+
+    public String Nome = "Bat";
+    public String Reino = "Animalia";
+    public String Filo = "Chordata";
+    public String Classe = "Mammalia";
+    public String Ordem = "Soricomorpha";
+    public String Família = "Soricidae";
+
+    //getter
+    public String getNome(){
+        return Nome;
+    }
+    public String getReino(){
+        return Reino;
+    }
+    public String getFilo(){
+        return Filo;
+    }
+    public String getClasse(){
+        return Classe;
+    }
+    public String getOrdem(){
+        return Ordem;
+    }
+
+    //setters
+
+
+    //comportamentos
+}
+
+
